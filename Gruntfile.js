c5c062f19c7f318cafe7ac8f7dd4fe0f0d402d3d
hanyen <me@hanyen.com>
2017-01-22 19:59:40 -0800

    Change port to 3000 and fix indentation in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-
       options: {
         separator: ';'
       },
       dist: {
-        src: ['public/client/**/*.js'],
-        dest: 'public/dist/<%= pkg.name %>.js'
+        src: ['./public/client/**/*.js'],
+        dest: './public/dist/<%= pkg.name %>.js',
       }
-          },
+    },
 
     mochaTest: {
       test: {
@@ -29,7 +28,6 @@ module.exports = function(grunt) {
     },
 
     uglify: {
-
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       },
@@ -38,22 +36,21 @@ module.exports = function(grunt) {
           'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
-          },
+    },
 
     eslint: {
       target: [
-
+        // Add list of files to lint here
         'Gruntfile.js',
         'app/**/*.js',
         'public/**/*.js',
         'lib/**/*.js',
         './*.js',
         'spec/**/*.js'
-              ]
+      ]
     },
 
     cssmin: {
-
       options: {
         keepSpecialComments: 0
       },
@@ -62,7 +59,7 @@ module.exports = function(grunt) {
           'public/dist/style.min.css': 'public/style.css'
         }
       }
-          },
+    },
 
     watch: {
       scripts: {
@@ -83,14 +80,13 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
-
         command: 'git push live master',
         options: {
           stdout: true,
           stderr: true,
           failOnError: true
         }
-              }
+      }
     },
   });
 
@@ -112,33 +108,31 @@ module.exports = function(grunt) {
   ////////////////////////////////////////////////////
 
   grunt.registerTask('test', [
-
     'eslint',
-        'mochaTest'
+    'mochaTest'
   ]);
 
   grunt.registerTask('build', [
-
-    'concat',
-    'uglify',
+    'concat', 
+    'uglify', 
     'cssmin'
-      ]);
+  ]);
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
-
+      // add your production server task here
       grunt.task.run([ 'shell:prodServer' ]);
-          } else {
+    } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
   grunt.registerTask('deploy', [
-
+    // add your deploy tasks here
     'test',
     'build',
     'upload'
-      ]);
+  ]);
 
 
 };
